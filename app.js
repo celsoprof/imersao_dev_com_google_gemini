5ee1490ec3f1521e62fa670a05339af2ec9a4be8
Prof Celso <celso.furtado@sp.senai.br>
2024-09-09 11:18:00 -0300

    Aula 05

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ function pesquisar(){
 
     let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    if (campoPesquisa == ""){
-        section.innerHTML = "<p>Nenhum atleta encontrado</p>"
+    if (!campoPesquisa){
+        section.innerHTML = "<p>Nenhum atleta encontrado. Digite uma frase ou palavra chave.</p>";
         return;
     }
 
-    campoPesquisa = campoPesquisa.toLowerCase()
+    campoPesquisa = campoPesquisa.toLowerCase();
 
     // Inicializa uma string vazia para armazenar a lista de resultados formatada em HTML.
     let lista = "";
     let titulo = "";
     let descricao = "";
+    let tags = "";
 
     // Itera sobre cada elemento "dado" no array "dados".
     for (let dado of dados){
         titulo = dado.titulo.toLowerCase();
         descricao = dado.descricao.toLowerCase();
-        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+        tags = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
             // Concatena HTML para criar um novo elemento de resultado com título, descrição e link.
             lista += `
             <div class="item-resultado">
@@ -33,6 +35,10 @@ function pesquisar(){
             `
         }
         
+    }
+
+    if (!lista){
+        lista = "<p>Nenhum atleta encontrado</p>";
     }
 
     // Atribui a string HTML completa à propriedade innerHTML da seção, substituindo o conteúdo existente.
--- dados.js
+++ dados.js
@@ -2,22 +2,26 @@ let dados = [
     {
       titulo: "Rayssa Leal",
       descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8"
     },
     {
       titulo: "Beatriz Souza",
       descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é um dos grandes nomes do judô brasileiro.",
-      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoca judoka"
     },
     {
       titulo: "Rebeca Andrade",
       descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances impecáveis. Ela é conhecida por sua força e elegância nas competições.",
-      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginasta ginástica ginastica ouro"
     },
     {
       titulo: "Augusto Akio",
       descricao: "Augusto Akio é um skatista brasileiro que vem se destacando no cenário nacional e internacional. Com um estilo único e inovador, ele é considerado uma das grandes promessas do skate brasileiro.",
-      link: "https://pt.wikipedia.org/wiki/Augusto_Akio"
+      link: "https://pt.wikipedia.org/wiki/Augusto_Akio",
+      tags: "skate japa sk8"
     }
   ];
 
